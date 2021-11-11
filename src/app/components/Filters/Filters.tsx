4f346340a450ca122b931c746e799bc15da2fbb7
Aleksandr Svetlakov <aleksandr.svetlakov@tranwall.com>
2021-11-11 18:12:32 +0700

    Added Filters component

---
+++ src/app/components/Filters/Filters.tsx
@@ -0,0 +1,17 @@
+import type { JSXElement } from 'solid-js';
+
+import css from './Filters.css';
+
+interface FiltersProps {
+  side: JSXElement;
+  children: JSXElement;
+}
+
+export function Filters(props: Readonly<FiltersProps>) {
+  return (
+    <div class={css.root}>
+      <div class={css.main}>{props.children}</div>
+      <div>{props.side}</div>
+    </div>
+  );
+}
