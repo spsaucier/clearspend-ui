be75724b0359e945d4634c4e1669423180955855
Stephen Saucier <stephen@saucier.co>
2021-12-14 13:00:11 -0500

    Fixed type

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -2,8 +2,8 @@ import { service } from 'app/utils/service';
 import type { User } from 'generated/capital';
 
 export async function login(username: string, password: string): Promise<User> {
-  const loginResponse = await service.post('/authentication/login', { username, password });
-  return loginResponse.data as User;
+  const loginResponse = await service.post<User>('/authentication/login', { username, password });
+  return loginResponse.data;
 }
 
 export async function logout() {
