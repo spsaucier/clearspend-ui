89a67e3e205ce0d6dd2d8d3632c3a6161d453a53
Aleksandr Svetlakov <aleksandr.svetlakov@clearspend.com>
2022-02-21 10:44:42 +0700

    Updated typings

--- src/generated/capital.ts
+++ src/generated/capital.ts
@@ -2379,6 +2379,10 @@ export interface CreateUpdateUserRecord {
   password?: string;
 }
 
+export interface NullableEncryptedString {
+  encrypted?: string;
+}
+
 export interface TestBusiness {
   business?: Business;
   users?: User[];
@@ -2397,7 +2401,19 @@ export interface BusinessOwner {
   type?: 'UNSPECIFIED' | 'PRINCIPLE_OWNER' | 'ULTIMATE_BENEFICIAL_OWNER';
   firstName?: NullableEncryptedString;
   lastName?: NullableEncryptedString;
+  title?: string;
+  relationshipOwner?: boolean;
+  relationshipRepresentative?: boolean;
+  relationshipExecutive?: boolean;
+  relationshipDirector?: boolean;
+  percentageOwnership?: number;
+  address?: Address;
+  taxIdentificationNumber?: NullableEncryptedString;
   email?: string;
+  phone?: string;
+
+  /** @format date */
+  dateOfBirth?: string;
   countryOfCitizenship?:
     | 'UNSPECIFIED'
     | 'ABW'
@@ -2647,21 +2663,9 @@ export interface BusinessOwner {
     | 'ZAF'
     | 'ZMB'
     | 'ZWE';
+  subjectRef?: string;
   knowYourCustomerStatus?: 'PENDING' | 'REVIEW' | 'FAIL' | 'PASS';
   status?: 'ACTIVE' | 'RETIRED';
-  title?: string;
-  relationshipOwner?: boolean;
-  relationshipRepresentative?: boolean;
-  relationshipExecutive?: boolean;
-  relationshipDirector?: boolean;
-  percentageOwnership?: number;
-  address?: Address;
-  taxIdentificationNumber?: NullableEncryptedString;
-  phone?: string;
-
-  /** @format date */
-  dateOfBirth?: string;
-  subjectRef?: string;
   stripePersonReference?: string;
 
   /** @format int64 */
@@ -2684,14 +2688,19 @@ export interface BusinessRecord {
   allocation?: Allocation;
 }
 
-export interface NullableEncryptedString {
-  encrypted?: string;
-}
-
 export interface ExpenseCategory {
   /** @format int32 */
   iconRef?: number;
   categoryName?: string;
+}
+
+export interface CodatBankAccount {
+  id?: string;
+  accountName?: string;
+}
+
+export interface CodatBankAccountsResponse {
+  results?: CodatBankAccount[];
 }
 
 export interface BusinessLimit {
