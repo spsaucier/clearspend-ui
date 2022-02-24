1a3db30e6a8451e4d6d71e0216458f0c7abf665b
Alex Liu <alexl@theodo.com>
2022-02-24 23:04:46 +0000

    Merged in AI-28_b (pull request #129)
    AI-28 add confirmation popover to unlink integration
    
    * add confirmation popover to unlink integration
    
    
    Approved-by: Stephen (Stevo) Saucier

--- src/accounting/containers/AccountingSettings/AccountingSettings.tsx
+++ src/accounting/containers/AccountingSettings/AccountingSettings.tsx
@@ -1,19 +1,49 @@
 import { Text } from 'solid-i18n';
+import { createSignal } from 'solid-js';
 
 import { Section } from 'app/components/Section';
 import { Button } from '_common/components/Button';
+import { Popover } from '_common/components/Popover';
 
 import css from './AccountingSettings.css';
 
 export function AccountingSettings() {
+  const [open, setOpen] = createSignal(false);
+
   return (
     <Section
       title={<Text message="Unlink Account" />}
       description={<Text message="Lorem ipsum dolor sit amet, consectetur adipiscing elit." />}
     >
-      <Button size="lg" icon="trash" class={css.unlink}>
-        <Text message="Unlink account" />
-      </Button>
+      <Popover
+        balloon
+        open={open()}
+        onClickOutside={() => setOpen(false)}
+        class={css.popoverRoot}
+        content={
+          <div>
+            <Text message="Lorem ipsum dolor sit amet, consectetur adipiscing elit." />
+            <div class={css.popoverButtons}>
+              <Button view="ghost" onClick={() => setOpen(false)}>
+                <Text message="Cancel" />
+              </Button>
+              <Button
+                class={css.popoverUnlink}
+                icon={{ name: 'confirm', pos: 'right' }}
+                onClick={() => {
+                  // TODO
+                }}
+              >
+                <Text message="Unlink now" />
+              </Button>
+            </div>
+          </div>
+        }
+      >
+        <Button size="lg" icon="trash" class={css.unlink} onClick={() => setOpen(true)}>
+          <Text message="Unlink account" />
+        </Button>
+      </Popover>
     </Section>
   );
 }
