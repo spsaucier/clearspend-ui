8483a76e794fffb3255af17926b61a890d9a10a1
Aleksandr Svetlakov <aleksandr.svetlakov@clearspend.com>
2022-02-28 16:34:02 +0700

    Exclude OTHER from spend controls

--- src/app/services/mcc.ts
+++ src/app/services/mcc.ts
@@ -3,5 +3,5 @@ import type { MccGroup } from 'transactions/types';
 import { service } from '../utils/service';
 
 export async function getMCCGroups() {
-  return (await service.get<readonly Readonly<MccGroup>[]>('/mcc-groups')).data;
+  return (await service.get<readonly Readonly<MccGroup>[]>('/mcc-groups')).data.filter((item) => item !== 'OTHER');
 }
