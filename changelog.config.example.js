6ad2e0441b1a546727088511bbb92d9cd920b16c
Stephen Saucier <stephen@saucier.co>
2022-03-17 13:34:43 -0400

    Added example jira-changelog config file

---
+++ changelog.config.example.js
@@ -0,0 +1,10 @@
+module.exports = {
+  jira: {
+    api: {
+      host: 'tranwall.atlassian.net',
+      email: '...@clearspend.com',
+      token: '', // Token from https://id.atlassian.com/manage-profile/security/api-tokens
+      options: {},
+    },
+  },
+};
