bf07d346743e329de024c35930c879ae5605a91d
Stephen Saucier <stephen@saucier.co>
2022-03-17 16:32:43 -0400

    [CAP-709] Update text & remove form for person to send onboarding to someone else

--- src/signup/components/SorryButForm/SorryBut.tsx
+++ src/signup/components/SorryButForm/SorryBut.tsx
@@ -1,3 +1,6 @@
+import { Show } from 'solid-js';
+import { Text } from 'solid-i18n';
+
 import { Form, FormItem, createForm } from '_common/components/Form';
 import { required } from '_common/components/Form/rules/required';
 import { Input } from '_common/components/Input';
@@ -24,26 +27,30 @@ export function SorryButForm(props: Readonly<SorryButFormProps>) {
 
   return (
     <div>
-      <Header>Sorry, but we need a company representative to open your account</Header>
+      <Header>
+        <Text message="Sorry, but we need a company representative to open your account" />
+      </Header>
       <Description>
-        Please select the roles that best describe you. A company representative is a business owner with at least 25%
-        ownership or a senior manager or other individual who controls and manages the business.
+        <Text message="A company representative is a business owner with at least 25% ownership, a senior manager, or any other individual that controls and manages the business." />
       </Description>
-      <Form onSubmit={wrapSubmit(onSubmit)}>
-        <FormItem label="Share with an owner or manager" error={errors().ownerEmail}>
-          <Input
-            name="email"
-            type="email"
-            value={values().ownerEmail}
-            error={Boolean(errors().ownerEmail)}
-            onChange={handlers.ownerEmail}
-          />
-        </FormItem>
-        <div style={{ height: '24px' }} />
-        <FlatButton type="primary" htmlType="submit">
-          Next
-        </FlatButton>
-      </Form>
+      {/* TODO: Re-add this form when CAP-718 done */}
+      <Show when={false}>
+        <Form onSubmit={wrapSubmit(onSubmit)}>
+          <FormItem label="Share with an owner or manager" error={errors().ownerEmail}>
+            <Input
+              name="email"
+              type="email"
+              value={values().ownerEmail}
+              error={Boolean(errors().ownerEmail)}
+              onChange={handlers.ownerEmail}
+            />
+          </FormItem>
+          <div style={{ height: '24px' }} />
+          <FlatButton type="primary" htmlType="submit">
+            <Text message="Next" />
+          </FlatButton>
+        </Form>
+      </Show>
     </div>
   );
 }
