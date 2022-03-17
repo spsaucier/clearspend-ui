89ffe551f18c9b4ae723c04493e4f067b6c5e9f3
Stephen Saucier <stephen@saucier.co>
2022-03-17 15:06:00 -0400

    Improved ticketIDPattern for jira-changlog

--- changelog.config.example.js
+++ changelog.config.example.js
@@ -1,10 +1,14 @@
+// @ts-ignore
 module.exports = {
   jira: {
     api: {
       host: 'tranwall.atlassian.net',
       email: '...@clearspend.com',
       token: '', // Token from https://id.atlassian.com/manage-profile/security/api-tokens
-      options: {},
+      options: {
+        ticketIDPattern: /\[([A-Z]+\-[0-9]+)\]/i,
+      },
     },
+    ticketIDPattern: /[A-Z]+\-[0-9]+/i,
   },
 };
