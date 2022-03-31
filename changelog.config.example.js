41e57df10c0a907b83dca91a54199af7442ea068
Stephen Saucier <stephen@saucier.co>
2022-03-31 14:25:29 -0400

    [CAP-822] Make signup form fields shorter

--- changelog.config.example.js
+++ changelog.config.example.js
@@ -5,9 +5,6 @@ module.exports = {
       host: 'tranwall.atlassian.net',
       email: '...@clearspend.com',
       token: '', // Token from https://id.atlassian.com/manage-profile/security/api-tokens
-      options: {
-        ticketIDPattern: /\[([A-Z]+\-[0-9]+)\]/i,
-      },
     },
     ticketIDPattern: /[A-Z]+\-[0-9]+/i,
   },
