10d2563d2521c2405c9bdc67624367ef7ea3a761
Max Hamilton <maxh@theodo.com>
2022-06-01 13:11:01 +0000

    Merged in feature/AI-375-accounting-setup-shows-classes (pull request #466)
    [AI-375] Added placeholder class dropdowns to the ClassesSetup page
    
    * Added placeholder class dropdowns to the ClassesSetup page
    
    
    Approved-by: Stephen (Stevo) Saucier

--- src/accounting/pages/ClassesSetup/ClassesSetup.tsx
+++ src/accounting/pages/ClassesSetup/ClassesSetup.tsx
@@ -4,6 +4,12 @@ import { Show } from 'solid-js';
 import { Page } from 'app/components/Page';
 import { CancelConfirmationButton } from 'accounting/components/CancelConfirmationButton';
 import { Button } from '_common/components/Button';
+import { useClasses } from 'accounting/stores/classes';
+import { Table, TableColumn } from '_common/components/Table';
+import type { CodatCategory } from 'generated/capital';
+import { Input } from '_common/components/Input';
+import { i18n } from '_common/api/intl';
+import { Icon } from '_common/components/Icon';
 
 import css from './ClassesSetup.css';
 
@@ -13,11 +19,41 @@ interface ClassesSetupProps {
 }
 
 export function ClassesSetup(props: ClassesSetupProps) {
+  const classes = useClasses();
+
+  const columns: Readonly<TableColumn<CodatCategory>>[] = [
+    {
+      name: 'className',
+      title: <Text message={'Class'} />,
+      render: (codatClass) => <div>{codatClass.categoryName}</div>,
+    },
+    {
+      name: 'clearspendClassName',
+      title: <Text message={'ClearSpend class'} />,
+      render: () => (
+        <Input
+          class={css.select}
+          placeholder={String(i18n.t('Assign class'))}
+          value={''}
+          suffix={<Icon name="chevron-down" size="sm" />}
+          onChange={() => {
+            return null;
+          }}
+          data-name="Search"
+        />
+      ),
+    },
+  ];
+
   return (
     <div class={css.root}>
       <Page contentClass={css.pageContent} title={<Text message="Set up your QuickBooks Online integration" />}>
         <div class={css.pageBody}>
-          <div></div>
+          <Show when={classes.data}>
+            <div class={css.table}>
+              <Table columns={columns} data={classes.data!} />
+            </div>
+          </Show>
           <div class={css.tableButtons}>
             <Show when={!!props.onCancel}>
               <CancelConfirmationButton onCancel={props.onCancel!} />
